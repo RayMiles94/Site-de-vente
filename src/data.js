23173072038f0d4fba66d4a086f163a85b13c666
raouf-slnee <raouf.jabri@slnee.com>
2020-01-27 13:52:48 +0100

    ...

--- src/data.js
+++ src/data.js
@@ -1,5 +1,18 @@
 var products = [];
 
+images = ['https://gloimg.zafcdn.com/zaful/pdm-product-pic/Clothing/2020/01/06/goods-first-img/1578952425488413422.jpg',
+'https://gloimg.zafcdn.com/zaful/pdm-product-pic/Clothing/2020/01/03/goods-first-img/1578964932853784717.jpg',
+'https://gloimg.zafcdn.com/zaful/pdm-product-pic/Clothing/2019/12/24/goods-first-img/1577248521626231874.jpg',
+'https://d1rkccsb0jf1bk.cloudfront.net/products/100021773/main/medium/1513584.jpg',
+'https://www.theperfumeshop.com/medias/sys_master/front-zoom/front-zoom/10672516988958/Gucci-Eau-de-Parfum-Gift-Set-for-her-3614228972048-Guilty.jpg',
+'',
+'',
+'',
+'',
+'',
+''
+];
+
 for (let index = 0; index < 10; index++) {
     var product = {};
     product.ref = "P" + index.toString();
