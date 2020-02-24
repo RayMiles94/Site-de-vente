606059c2ed9809201d39799276c458bc5f17ecec
raouf-slnee <raouf.jabri@slnee.com>
2020-02-24 10:10:15 +0100

    [FIX] ...

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,12 @@
-serialize = function(obj) {
+serialize = function (obj) {
     var str = [];
-    for (var p in obj)
-      if (obj.hasOwnProperty(p)) {
-        str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
-      }
+    for (var p in obj) 
+        if (obj.hasOwnProperty(p)) {
+            str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+        }
+    
     return str.join("&");
-  }
+}
 
 $('#senddata').click(function () {
     var login = document.getElementById('inputlogin').value;
@@ -13,9 +14,9 @@ $('#senddata').click(function () {
     var confirm = document.getElementById('inputEmailConfirm').value;
     var password = document.getElementById('inputPassword').value;
     const query = {
-        login : login,
-        email : email,
-        password : password
+        login: login,
+        email: email,
+        password: password
     }
     var mx = $.ajax({
         url: "/usercheck",
@@ -26,10 +27,9 @@ $('#senddata').click(function () {
             login: login
         },
         success: function (data) {
-            if( (password==confirm) && (data.reponse=="not") ){
-                window.location = "/loginsucc?"+serialize(query);
-            }
-            else {
+            if ((password == confirm) && (data.reponse == "not")) {
+                window.location = "/loginsucc?" + serialize(query);
+            } else {
                 alert("This User is already exist");
                 document.getElementById('error').innerHTML = "This User is already exist";
             }
@@ -43,28 +43,27 @@ $('#senddata').click(function () {
 });
 
 $("#submitlogin").click(function () {
-   var login =  document.getElementById('loginbinputEmail').value;
-   var mp = document.getElementById('logininputPassword').value;
-   const user = {
-       login : login,
-       password: mp
-   };
-   $.ajax({
-    url: "/userchecklogin",
-    method: "GET",
-    dataType: 'json',
-    data: user,
-    success : function(data){
-        if(data.reponse=="found"){
-            window.location = "/signformsend?"+serialize(data.data);
+    var login = document.getElementById('loginbinputEmail').value;
+    var mp = document.getElementById('logininputPassword').value;
+    const user = {
+        login: login,
+        password: mp
+    };
+    $.ajax({
+        url: "/userchecklogin",
+        method: "GET",
+        dataType: 'json',
+        data: user,
+        success: function (data) {
+            if (data.reponse == "found") {
+                window.location = "/signformsend?" + serialize(data.data);
+            } else {
+                alert("This User is Not exist");
+            }
+        },
+        error: function (data) {
+            console.log(data);
+            window.alert(data);
         }
-        else {
-            alert("This User is Not exist");
-        }
-    },
-    error: function (data) {
-        console.log(data);
-        window.alert(data);
-    }
-   })
+    })
 });
